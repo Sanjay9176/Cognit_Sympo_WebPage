b0c19b4a4c3aa295d2b46f8862a23b6e38efe6ca
danielace1 <danielsudharsan1805@gmail.com>
2024-09-25 00:29:55 +0530

    feat: design home banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 const App = () => {
-  return <div>CSE SYMPOSIUM</div>;
+  return (
+    <div className="">
+      <div className="bg-[url('/home-bg.jpeg')] bg-cover object-cover min-h-screen bg-no-repeat flex justify-center items-center text-center">
+        <div className="">
+          <h1 className="text-white text-8xl font-bold text-stroke-2-cse-violet mb-8">
+            SYNERGY’24
+          </h1>
+          <h2 className="text-white text-5xl font-bold text-stroke-1-cse-violet mb-12">
+            LET’S VIBE WITH US
+          </h2>
+          <h3 className="text-white text-5xl text-stroke-1-cse-violet font-bold">
+            THE GREAT EVENTS STARTS FORM
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textStrokeWidth: {
+        1: "1px",
+        2: "2px",
+        3: "3px",
+        4: "4px",
+        5: "5px",
+      },
+
+      colors: {
+        "cse-violet": "#FF2EFF",
+        "cse-cyan": "#3DF6FF",
+      },
+    },
+    container: {
+      center: true,
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities, theme }) {
+      const strokeWidths = theme("textStrokeWidth");
+      const colors = theme("colors");
+
+      const newUtilities = Object.entries(strokeWidths).reduce(
+        (acc, [key, width]) => {
+          Object.entries(colors).forEach(([colorKey, colorValue]) => {
+            acc[`.text-stroke-${key}-${colorKey}`] = {
+              "-webkit-text-stroke-width": width,
+              "-webkit-text-stroke-color": colorValue,
+            };
+          });
+          return acc;
+        },
+        {}
+      );
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };
